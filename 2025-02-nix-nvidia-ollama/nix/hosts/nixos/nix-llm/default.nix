f8c5e2ff9259a5e23713c2056ae426d970fc591e
Evan Schalton <Evan.Schalton@Gmail.Com>
2025-04-14 22:04:17 -0400

    Update default.nix

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -14,19 +14,29 @@
     efi.canTouchEfiVariables = true;
   };
 
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableNvidia = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+  };
+
   # Network configuration
   networking = {
     firewall.enable = false;
     hostName = "nix-llm";
-    interfaces.ens18 = {
+    interfaces.enp14s0 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "10.42.37.100";
+        address = "llama.lan";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "192.168.212.1";
+    nameservers = [ "192.168.212.1" ];
   };
 
   # System localization
